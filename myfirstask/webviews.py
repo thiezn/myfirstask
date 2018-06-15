29c0f3ba8cbd4e671d86282ebf7a612413c4b155
Mortimer <Mathijs.Mortimer@rabobank.com>
2018-06-15 15:45:49 +0200

    Added home and about templates

--- myfirstask/webviews.py
+++ myfirstask/webviews.py
@@ -5,13 +5,21 @@ Holds all the website views
 """
 import aiohttp_jinja2
 
+users = [
+    {
+        "name": "test",
+        "occupation": "IT guy",
+        "skillset": "Kubernetes"
+    }
+]
+
 
 @aiohttp_jinja2.template('home.html')
 async def home_page(request):
     """Serving the main webpage showing backend data"""
-    return {'users': "user"}
+    return {'home_page_active': True}
 
 
 @aiohttp_jinja2.template('about.html')
 async def about_page(request):
-    return {'about_page_active': True}
+    return {'about_page_active': True, 'users': users}
